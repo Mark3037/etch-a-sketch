345a2340b09d9fcbd0607cbc69acdcc1cb11c5a7
Mark Huber <markhuber0907@outlook.com>
2023-05-12 16:06:15 -0700

    Add HTML boilerplate and javascript pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+'create a 16x16 grid of divs using DOM manipulation'
+    'put grids into div.container'
+
+'create hover effect so divs change color when mouse passes over them'
+    'this should create a trail like a pen would'
+
+'make a button on top of the screen'
+    'this button will prompt users for the number of squares for a new grid'
+    'this number should not exceed 100'
+    'the new grid should take up the same amount as the old one'
+
+// optional extra credit:
+//    when passing through a div, try changing it to a random rgb value
+//    then maybe have each pass add 10% opacity
