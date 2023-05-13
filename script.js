44baa05e1a9f2507c074e6b515837e3f432f61d3
Mark Huber <markhuber0907@outlook.com>
2023-05-12 17:20:57 -0700

    Add button that prompts user for grid size

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const container = document.querySelector("div.container")
 
-function gridSize(size) {
+function gridSize() {
+    size = prompt("How many rows/columns?");
+    if(size > 100) {
+        alert("Needs to be lower than 100");
+        return gridSize()
+    }
     for(let x = 0; x < size; x++) {
-        newRow = document.createElement("div")
-        newRow.className = "row"
+        newRow = document.createElement("div");
+        newRow.className = "row";
         for(let y = 0; y < size; y++) {
-            newCol = document.createElement("div")
-            newCol.className = "column"
-            newRow.appendChild(newCol)
+            newCol = document.createElement("div");
+            newCol.className = "column";
+            newRow.appendChild(newCol);
         }
-        container.appendChild(newRow)
+        container.appendChild(newRow);
     }
 }
+
+const sizeButton = document.querySelector("button.size");
+sizeButton.addEventListener("click", () => gridSize()) ;
 
 'create hover effect so divs change color when mouse passes over them'
     'this should create a trail like a pen would'
